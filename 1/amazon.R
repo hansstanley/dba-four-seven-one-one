75214ebc55f0873cffc1f3c52fd4b0023c3736a0
hansstanley <nusstuhss@gmail.com>
2024-02-13 13:59:03 +0800

    update degree and closeness centrality

--- 1/amazon.R
+++ 1/amazon.R
@@ -13,8 +13,7 @@ library(tidyr)
 
 ### Part I: visualization                ###
 # read in the nodes and edges data
-edges_0302 <- read.table("amazon0302.txt", header=F, skip=4, sep="\t")
-edges_0601 <- read.table("amazon0601.txt", header=F, skip=4, sep="\t")
+edges <- read.table("amazon0302.txt", header=F, skip=4, sep="\t")
 
 # names of columns to read from nodes metadata
 columns <- c("Id", "title", "group")
@@ -25,7 +24,8 @@ raw_table <- read.table(
   col.names=c("Raw")
 )
 # split each line into parts using ":" as the delimiter
-raw_split <- strsplit(raw_table$Raw, ":", fixed=T)
+# this takes a short while
+raw_split <- strsplit(raw_table$Raw, ":\\s*")
 # filter lines where the first part is one of the column names
 raw_split <- raw_split[lapply(raw_split, "[[", 1) %in% columns]
 
@@ -33,7 +33,7 @@ raw_split <- raw_split[lapply(raw_split, "[[", 1) %in% columns]
 keys <- lapply(raw_split, "[[", 1)
 # vals will correspond to the data in each row of the `nodes_df` data frame;
 # concatenate the rest of the parts together with ":"
-vals <- lapply(raw_split, function(parts) paste(parts[-1], collapse = ":"))
+vals <- lapply(raw_split, function(parts) paste(parts[-1], collapse = ": "))
 # optional, trim the white space from the values (quite slow)
 # vals <- lapply(vals, trimws)
 # create key-value data frame
@@ -56,6 +56,7 @@ nodes_df <- pivot_wider(
 )
 # remove the `row` column
 nodes_df <- nodes_df[, columns]
+# convert `Id` column to integer to match edges data frame
 nodes_df$Id <- as.integer(nodes_df$Id)
 
 
@@ -64,7 +65,7 @@ head(nodes_df)
 # the node data provides the index of the nodes
 # as well as other attributes
 
-head(edges_0302)
+head(edges)
 # edges data include the indices of two nodes
 # indicating the two nodes that form an edge
 
@@ -76,13 +77,13 @@ head(edges_0302)
 # first column of the node data should be the
 # ID of that node.
 # We set directed=F for undirected graphs
-G_full = graph_from_data_frame(edges_0302, directed=F, nodes_df)
+G_full = graph_from_data_frame(edges, directed=F, nodes_df)
 
-# prune nodes with degree less than `max_degree` so that
+# prune nodes with degree less than `deg_threshold` so that
 # graphs can be rendered in a reasonable amount of time
-max_degree <- 50
+deg_threshold <- 40
 G_degrees <- degree(G_full, mode = "all")
-G_orphans <- which(G_degrees < max_degree)
+G_orphans <- which(G_degrees < deg_threshold)
 G <- delete_vertices(G_full, G_orphans)
 
 # plotting graphs
@@ -123,7 +124,7 @@ get.colors = function(cols, x) {
 # You don't need to understand or change get.colors
 # to use it when plotting:
 plot(G, layout=mylayout, vertex.label=NA, vertex.size=2*sqrt(degree(G)),
-     vertex.color = get.colors(c("white", "purple"), 2^degree(G)))
+     vertex.color = get.colors(c("white", "purple"), degree(G)^2))
 # 2^degree(G) emphasizes the nodes with the highest degrees
 ### END ALERT ###
 
@@ -132,29 +133,36 @@ plot(G, layout=mylayout, vertex.label=NA, vertex.size=2*sqrt(degree(G)),
 # From now on, we are only going to work with
 # students who are in the largest connected
 # component of the graph
-comp = components(G_full)
+
+# we use `G` (instead of `G_full`) for better performance in plotting,
+# but `G_full` when computing actual degree centrality
+comp = components(G)
 # inspect the membership
 comp$membership
 table(comp$membership)
 # only keep those that are in the largest
 in.max.comp = (comp$membership == 1)
-sg = induced_subgraph(G_full, in.max.comp)
+sg = induced_subgraph(G, in.max.comp)
 # get the sub layout
 sg_mylayout = mylayout[in.max.comp,]
-plot(sg, layout=sg_mylayout)
+plot(sg, layout=sg_mylayout, vertex.label=NA, vertex.size=2*sqrt(degree(sg)))
 
 V(sg)[degree(sg) == max(degree(sg))]
 
 # Metric 1: Degree Centrality
-# This is just the number of edges coming in/out
-# of each of the node. This is provided directly
-# from the graph, no calculation required.
-sg.degree <- data.frame(deg=degree(sg))
-sg.degree$Id <- row.names(sg.degree)
-sg.degree <- sg.degree[order(sg.degree$deg, decreasing = T),]
-sg.degree.100 <- sg.degree[1:100,]
-sg.degree.100 <- merge(sg.degree.100, nodes_df, by="Id")
-sg.degree.100 <- sg.degree.100[order(sg.degree.100$deg, decreasing = T),]
+G_full.degree <- data.frame(deg=degree(G_full))
+G_full.degree$Id <- row.names(G_full.degree)
+# sort by degree in decreasing order to obtain the top 100 highest degrees
+G_full.degree <- G_full.degree[order(G_full.degree$deg, decreasing = T),]
+G_full.degree.100 <- G_full.degree[1:100,]
+# merge with the metadata table to identify each node
+G_full.degree.100 <- merge(G_full.degree.100, nodes_df, by="Id")
+# sort by descending degree again after merging
+G_full.degree.100 <- G_full.degree.100[
+  order(G_full.degree.100$deg, decreasing = T),
+]
+# export as csv
+write.csv(G_full.degree.100, "G_full.degree.100.csv", row.names = F)
 # We can plot by degree (which we already did!)
 # The eye measures the size of a point by its
 # area but vertex.size scales the radius of the
@@ -164,27 +172,56 @@ sg.degree.100 <- sg.degree.100[order(sg.degree.100$deg, decreasing = T),]
 # should multiply the square root by whatever
 # value is needed to make the points visually
 # appealing.
-plot(sg, layout=mylayout, vertex.label=NA,
-     vertex.size=2*sqrt(degree(sg)))
+plot(G, layout=mylayout, vertex.label=NA,
+     vertex.size=2*sqrt(degree(G)),
+     vertex.color = get.colors(c("white", "purple"), degree(G)))
 
 # Metric 2: Closeness Centrality
 # How "central" a node is in a network
 # = 1 / sum(shortest path lengths to other nodes)
-# Recall that closeness only works on connected
-# components, so we need to use the 'sg' graph!
-sg_cl = closeness(sg)
-summary(sg_cl)
+# Recall that closeness only works on connected components,
+# so we find the largest connected component in `G_full`
+comp_full = components(G_full)
+comp_full$membership
+# only keep those that are in the largest
+in.max.comp_full = (comp_full$membership == 1)
+G_conn = induced_subgraph(G_full, in.max.comp_full)
+
+G_conn_cl = closeness(G_conn, cutoff = 3)
+summary(G_conn_cl)
+
+G_sub.closeness <- data.frame(cls=G_conn_cl)
+G_sub.closeness$Id <- row.names(G_sub.closeness)
+G_sub.closeness <- G_sub.closeness[order(G_sub.closeness$cls, decreasing = T),]
+G_sub.closeness.100 <- G_sub.closeness[1:100,]
+G_sub.closeness.100 <- merge(G_sub.closeness.100, nodes_df, by="Id")
+G_sub.closeness.100 <- G_sub.closeness.100[
+  order(G_sub.closeness.100$cls, decreasing = T),
+]
+# export as csv
+write.csv(G_sub.closeness.100, "G_sub.closeness.100.csv", row.names = F)
 # Basic plot
 plot(sg, layout=sg_mylayout, vertex.label=NA, vertex.size=10000*(sg_cl))
 # Or with colors!
-plot(sg, layout=sg_mylayout, vertex.label=NA, vertex.size=100*sqrt(sg_cl), 
-     vertex.color = get.colors(c("white", "purple"), closeness(sg)^10))
+plot(sg, layout=sg_mylayout, vertex.label=NA, vertex.size=200*sqrt(sg_cl), 
+     vertex.color = get.colors(c("white", "purple"), sg_cl^10))
 
 # Metric 3: Betweenness Centrality
 # Number of shortest paths in the network in
 # which this node appears
 bn = betweenness(G)
 summary(bn)
+
+G_sub.between <- data.frame(btw=betweenness(G))
+G_sub.between$Id <- row.names(G_sub.between)
+G_sub.between <- G_sub.between[order(G_sub.between$btw, decreasing = T),]
+G_sub.between.100 <- G_sub.between[1:100,]
+G_sub.between.100 <- merge(G_sub.between.100, nodes_df, by="Id")
+G_sub.between.100 <- G_sub.between.100[
+  order(G_sub.between.100$btw, decreasing = T),
+]
+# export as csv
+write.csv(G_sub.between.100, "G_sub.between.100.csv", row.names = F)
 # Try basic plot:
 plot(G, layout=mylayout, vertex.label=NA, vertex.size=sqrt(bn))
 # Not good! We need to shrink them a bit
@@ -200,9 +237,21 @@ pr = page_rank(G)$vector
 summary(pr)
 # Plot it
 plot(G, layout=mylayout, vertex.label=NA, vertex.size=100*sqrt(pr),
-     vertex.color = get.colors(c("white", "purple"), pr))
+     vertex.color = get.colors(c("white", "purple"), pr^2))
 
+pr_full = page_rank(G_full)$vector
+summary(pr_full)
+G_full.pagerank <- data.frame(pr=pr_full)
+G_full.pagerank$Id <- row.names(G_full.pagerank)
+G_full.pagerank <- G_full.pagerank[order(G_full.pagerank$pr, decreasing = T),]
+G_full.pagerank.100 <- G_full.pagerank[1:100,]
+G_full.pagerank.100 <- merge(G_full.pagerank.100, nodes_df, by="Id")
+G_full.pagerank.100 <- G_full.pagerank.100[
+  order(G_full.pagerank.100$pr, decreasing = T),
+]
 
+# export as csv
+write.csv(G_full.pagerank.100, "G_full.pagerank.100.csv", row.names = F)
 
 ###     PART III: Community Detection     #####
 # igraph makes this pretty easy too
